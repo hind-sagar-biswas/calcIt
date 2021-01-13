160cc8f0473889bf0fe182ea8f49089a56425794
Hind Sagar Biswas <hindsbhk@gmail.com>
2021-01-13 16:56:55 +0600

    Added New Functions
    Added New Functions and changed the previous script.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,76 +1,191 @@
 var result;
-var math = "";
-var math_;
+var inputCurrent = "";
+var input1;
 var operator;
-var triggered;
-var i = 0;
+var triggered = false;
+var answer;
+var clickCount = 0;
 
-function calc(a) {
-		if(Number.isInteger(a) || a == '.') {
+function calc(value) {
+		if(!isNaN(value) || value == '.') {
 				if(triggered) {
-					  if(i == 2) {
-					  		math_ = result;
+					  if(clickCount == 2) {
+					  		input1 = result;
 					  }
-						math = math + a.toString();
-						document.getElementById('calc').innerHTML = math;
+						inputCurrent = inputCurrent + value.toString();
+						document.getElementById('calc').innerHTML = inputCurrent;
 						switch(operator) {
 								case "+":
-										result = math_ + Number(math);
+										result = input1 + Number(inputCurrent);
 										break;
 								case "-":
-										result = math_ - Number(math);
+										result = input1 - Number(inputCurrent);
 										break;
 								case "×":
-										result = Number(math) * math_;
+										result = input1 * Number(inputCurrent);
 										break;
 								case "÷":
-										result = math_ / Number(math);
+										result = input1 / Number(inputCurrent);
 										break;
 								case "rem":
-										result = math_ % Number(math);
+										result = input1 % Number(inputCurrent);
 										break;
 								case "^":
-										result = Math.pow(math_, Number(math));
+										result = Math.pow(input1, Number(inputCurrent));
 										break;
 						}
 						document.getElementById('result').innerHTML = result;
-						if(i == 2) {
-								i = 1;
+						if(clickCount == 2) {
+								clickCount = 1;
 						}
 				} else {
-						math = math + a.toString()
-						document.getElementById('calc').innerHTML = math;}
+						inputCurrent = inputCurrent + value.toString()
+						document.getElementById('calc').innerHTML = inputCurrent;}
 						
-		} else if(a == '='){
+		} else if(value == '='){
 				triggered = false;
-				document.getElementById('calc').innerHTML = math_.toString() + " " + operator + " " + math + ' = ' +result;
-				math = "";
-				math_ = 0;
+				document.getElementById('calc').innerHTML = input1.toString() + " " + operator + " " + inputCurrent + ' = ' +result;
+				inputCurrent = "";
+				input1 = 0;
+				answer = result;
 				result = 0;
 				operator = "";
 				i = 0;
 		}  else {
 				triggered = true;
-				operator = a;
-				math_ = Number(math);
-				i++;	document.getElementById('result').innerHTML = math;
-				math = "";
-				document.getElementById('calc').innerHTML = a;
+				operator = value;
+				input1 = Number(inputCurrent);
+				if(clickCount == 0) {
+						result = Number(inputCurrent);
+				}
+				clickCount++;	document.getElementById('result').innerHTML = result;
+				inputCurrent = "";
+				document.getElementById('calc').innerHTML = value;
 		}
 }
 
 function ac() {
 		document.getElementById('calc').innerHTML = '00';
 		document.getElementById('result').innerHTML = "00";
-		math = "";
-		math_ = 0;
+		inputCurrent = "";
+		input1 = 0;
 		result = 0;
 		operator = "";
 		triggered = false;
-		i = 0;
+		clickCount = 0;
+		answer = 0;
 }
 
 function z() {
 		calc(0);
 		calc(0);
+		}
+
+function cons(value) {
+		var constantValue;
+		switch(value) {
+				case "π":
+				  constantValue = Math.PI;
+				  break;
+				case "e":
+				  constantValue = Math.E;
+				  break;
+				case "g":
+				  constantValue = 9.78033;
+				  break;
+				case "G":
+				  constantValue = 6.67 * Math.pow(10, -11);
+				  break;
+				case "c":
+				  constantValue = 299792458;
+				  break;
+		}
+		calc(constantValue);
+		document.getElementById('calc').innerHTML = value;
+}
+
+function expo(base, power, op) {
+		var o = "";
+		var e;
+		
+		
+						e = (base * base).toString();
+						if(triggered) {
+								switch(operator) {
+										case "+":
+												o = "-";
+												break;
+										case "-":
+												o = "+";
+												break;
+						  			case "×":
+												o = "÷";
+												break;
+										case "÷":
+												o = "×";
+												break;
+										}
+						calc(o);
+						calc(base);
+						calc(op);
+						} else {
+								ac();
+						}
+					  calc(e);	document.getElementById('calc').innerHTML = base + '<sup>' + power + '</sup> (' + inputCurrent + ')';
+}
+
+function func(value) {
+		switch(value) {
+				case '-':
+						inputCurrent = (Number(inputCurrent) * -1).toString();
+						document.getElementById('calc').innerHTML = inputCurrent;
+						break;
+				case 'sqr':
+						expo(Number(inputCurrent), 2, operator);
+						break;
+				case 'cube':
+						expo(Number(inputCurrent), 3, operator);
+						break;
+				case 'x!':
+						var o = "";
+						var op = operator;
+						var h = inputCurrent;
+						var j = 1;
+						for(var i = 1; i <= Number(inputCurrent); i++) {
+								j = j * i;
+						}
+						if(triggered) {
+								switch(operator) {
+										case "+":
+												o = "-";
+												break;
+										case "-":
+												o = "+";
+												break;
+						  			case "×":
+												o = "÷";
+												break;
+										case "÷":
+												o = "×";
+												break;
+										}
+						calc(o);
+						calc(h);
+						calc(op);
+						} else {
+								ac();
+						}
+					  calc(j);
+					  document.getElementById('calc').innerHTML = h + '! (' + j + ')';
+						break;
+		}
+}
+
+function ans() {
+		calc(answer);
+		document.getElementById('calc').innerHTML = 'ANS (' + answer + ')';
+}
+
+function eng() {
+		
 }
